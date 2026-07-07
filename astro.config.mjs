3b298b77e2b2b6e349e2e8bf05bd0fc30a407f44
lunaidev <saihry.2222@gmail.com>
2026-07-06 21:02:51 -0400

    feature: implementación de SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jpvs-web.renkaidev.workers.dev/',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
